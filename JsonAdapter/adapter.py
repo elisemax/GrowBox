188157bb9b7f125f53237787adbb61d4928819f0
Comodo13 <dimanazdar@gmail.com>
2022-04-16 17:13:26 +0200

    pppp

--- JsonAdapter/adapter.py
+++ JsonAdapter/adapter.py
@@ -6,10 +6,11 @@ class Adapter:
     def __init__(self):
         self.json_path = 'JsonAdapter/data.json'
         self.data = self.Json_Obj()
-        self.temperature = self.data['temperatureSensor']
+        self.temperature = self.data['temperature']
         self.ledStatus = self.data['ledStatus']['ledOn']
-        self.ledValue = self.data['ledStatus']['intensive']
-        self.ledType = self.data['ledStatus']['typeLigth']
+        self.ledTimerOn = self.data['ledStatus']['lightsOn']
+        self.ledTimerOff = self.data['ledStatus']['lightsOff']
+        self.timer = 3.12
     def Json_Obj(self):
         with open(self.json_path) as f:
             data = json.load(f)
@@ -19,9 +20,9 @@ class Adapter:
             json.dump(self.data,f)
     def temperatureUpdate(self):
         #self.temperature = sensors.temperature.get_temperature1()
-        self.data['temperatureSensor'] = sensors.temperature.get_temperature2()
+        self.data['temperature'] = sensors.temperature.get_temperature1()
     def get_temperature(self):
-        self.temperature = self.data['temperatureSensor']
+        self.temperature = self.data['temperature']
         return self.temperature
     def ledTurnOff(self):
         self.data['ledStatus']['ledOn'] = sensors.led.LedOff()
@@ -30,3 +31,10 @@ class Adapter:
     def get_ledStatus(self):
         self.ledStatus = self.data['ledStatus']['ledOn']
         return self.ledStatus
+    def ledTimerControl(self,timer):
+        jsonTimer = json.loads(timer)
+        self.ledTimerOn = jsonTimer['lightsOn']
+        self.ledTimerOff = jsonTimer['lightsOff']
+    def ledUpdateStatus(self):
+        self.data['ledStatus']['lightsOn'] = self.ledTimerOn
+        self.data['ledStatus']['lightsOff'] = self.ledTimerOff
--- sensors/led.py
+++ sensors/led.py
@@ -1,11 +1,14 @@
-import re
+import time
+import datetime
 
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
 
-def LedOn():
+def LedOn(timer):
     return 'true'
 def LedOff():
     return 'false'
-def get_intensive():
-    return 'someValue'
-def getTypeLight():
-    return 'Ultra'
