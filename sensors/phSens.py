ab58b1417ab8689dd0684e7b83ad71a4abaa6f2a
dzmitry.rabotkin@woltair.cz <hot999alk13>
2022-04-23 16:03:06 +0200

    ph

---
+++ sensors/phSens.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+''' Raspberry Pi, ADS1115, PH4502C Calibration '''
+from audioop import avg
+import board
+import busio
+import time
+import sys
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Setup 
+
+def read_voltage():
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c)
+    channel = AnalogIn(ads, ADS.P0)
+    buf = list()
+        
+    for i in range(10): # Take 10 samples
+        buf.append(channel.voltage)
+        buf.sort() # Sort samples and discard highest and lowest
+        buf = buf[2:-2]
+        avg = (sum(map(float,buf))/6) # Get average value from remaining 6
+
+        print(round(avg,2),'V')
+        time.sleep(1)
+    
+    return round(avg,2)
+
+   
+   
