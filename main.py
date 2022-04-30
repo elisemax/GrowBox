20f6c7e8b32ee1a409b5274a924b6f806a38ad32
dzmitry.rabotkin@woltair.cz <hot999alk13>
2022-04-30 15:54:34 +0200

    h

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import requests
 import JsonAdapter.adapter
 import time
 
-try:
-    systemGrow = JsonAdapter.adapter.Adapter()
-    while(True):   
 
+systemGrow = JsonAdapter.adapter.Adapter()
+while(True): 
+    try:  
         #localtime = time.localtime()
         #result = time.strftime("%H:%M:%S",localtime)
         #print(result)
@@ -25,5 +25,18 @@ try:
         systemGrow.get_ledStatus()
         systemGrow.tdsUpdate()
         print(rGet.status_code)
-except:
-    print("lost connection to server trying again in 3 seconds")    
+    except:
+        print("lost connection to server trying again in 3 seconds")   
+
+    finally: 
+        time.sleep(1)
+        rGet = requests.get('http://192.168.0.10:8080/grow/status',json=systemGrow.Json_Obj())
+        tJsonObj = rGet.text
+        systemGrow.phUpdate()
+        systemGrow.Json_Upd()
+        systemGrow.temperatureUpdate()
+        time.sleep(3)
+        systemGrow.ledUpdateStatus(tJsonObj)
+        systemGrow.get_ledStatus()
+        systemGrow.tdsUpdate()
+        print(rGet.status_code)
