82e499e1726b60f53fb3eb1d9c23d628ebd24f7f
dzmitry.rabotkin@woltair.cz <hot999alk13>
2022-04-30 16:19:04 +0200

    d

--- main.py
+++ main.py
@@ -25,19 +25,8 @@ while(True):
         systemGrow.get_ledStatus()
         systemGrow.tdsUpdate()
         print(rGet.status_code)
-    except:
-        print("lost connection to server trying again in 3 seconds")   
-        time.sleep(1)
-        rGet = requests.get('http://192.168.0.10:8080/grow/status',json=systemGrow.Json_Obj())
-        tJsonObj = rGet.text
-        systemGrow.phUpdate()
-        systemGrow.Json_Upd()
-        systemGrow.temperatureUpdate()
-        time.sleep(3)
-        systemGrow.ledUpdateStatus(tJsonObj)
-        systemGrow.get_ledStatus()
-        systemGrow.tdsUpdate()
-        print(rGet.status_code)
+    except Exception:
+        pass
 
      
         
