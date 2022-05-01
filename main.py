e03da33e6de59be0a86ee1ada0d1521d036ca728
dzmitry.rabotkin@woltair.cz <hot999alk13>
2022-05-01 13:00:57 +0200

    п

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ while(True):
         print(rGet.status_code)
         g = git.cmd.Git('https://github.com/elisemax/GrowBox.git')
         g.pull()
-        print('no gog')
+    
         
     except Exception:
         pass
