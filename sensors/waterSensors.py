dc7440af1515e6e400b3d81390bf11b36193655e
Dima <dzmitry.rabotkin@woltair.cz>
2022-05-14 15:19:46 +0200

    ff

--- sensors/waterSensors.py
+++ sensors/waterSensors.py
@@ -39,11 +39,9 @@ class WaterSensors:
         
 
         buf = list()
-        
         for i in range(10): # Take 10 samples
             buf.append(channel.voltage)
-            print( "PH voltage:")
-            print(channel.voltage)
+            
         buf.sort() # Sort samples and discard highest and lowest
         buf = buf[2:-2]
         avg = (sum(map(float,buf))/6) # Get average value from remaining 6
