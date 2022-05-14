fa1623a868f564681b6c59c6403d0ceb1b13c736
Dima <dzmitry.rabotkin@woltair.cz>
2022-05-14 16:36:55 +0200

    fgchgj

--- sensors/waterSensors.py
+++ sensors/waterSensors.py
@@ -34,7 +34,7 @@ class WaterSensors:
 
     def ph_read_voltage():
         i2c = busio.I2C(board.SCL, board.SDA)
-        ads = ADS.ADS1015(i2c,gani=2/3,data_rate=128)
+        ads = ADS.ADS1015(i2c,gain=2/3,data_rate=128)
         channel = AnalogIn(ads, ADS.P0)
         
 
