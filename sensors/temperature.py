a23596831226085f7c29172b6caa7f7de31fa358
Max <max11cz@seznam.cz>
2022-05-15 16:38:38 +0200

    fasd

--- sensors/temperature.py
+++ sensors/temperature.py
@@ -3,7 +3,3 @@ from w1thermsensor import W1ThermSensor
 def get_temperature():
     sensor = W1ThermSensor()
     return sensor.get_temperature()
-#def get_temperature1():
-#    return "28"
-#def get_temperature2():
-#    return "54"
