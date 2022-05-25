3fe720ba40b778c4b8f2b06226636a82a9851cfa
Dima <dzmitry.rabotkin@woltair.cz>
2022-05-25 22:19:49 +0200

    x

--- sensors/led.py
+++ sensors/led.py
@@ -20,19 +20,33 @@ class Led:
         GPIO.setmode(GPIO.BCM)
         OutputPin1 = 13
         OutputPin2 = 19
+        OutputPin3 = 5
+        OutputPin4 = 6
         GPIO.setup(OutputPin1, GPIO.OUT)
         GPIO.setup(OutputPin2, GPIO.OUT)
+        GPIO.setup(OutputPin3, GPIO.OUT)
+        GPIO.setup(OutputPin4, GPIO.OUT)
         GPIO.setwarnings(False)
         GPIO.output(OutputPin1, GPIO.HIGH)
         GPIO.output(OutputPin2, GPIO.HIGH)
+        GPIO.output(OutputPin3, GPIO.HIGH)
+        GPIO.output(OutputPin4, GPIO.HIGH)
         print("LedOn")
     def LedOn(self):
         GPIO.setmode(GPIO.BCM)
         OutputPin1 = 13
         OutputPin2 = 19
+        OutputPin3 = 5
+        OutputPin4 = 6
         GPIO.setup(OutputPin1, GPIO.OUT)
         GPIO.setup(OutputPin2, GPIO.OUT)
+        GPIO.setup(OutputPin3, GPIO.OUT)
+        GPIO.setup(OutputPin4, GPIO.OUT)
         GPIO.setwarnings(False)
         GPIO.output(OutputPin1, GPIO.LOW)
         GPIO.output(OutputPin2, GPIO.LOW)
+        GPIO.output(OutputPin3, GPIO.LOW)
+        GPIO.output(OutputPin4, GPIO.LOW)
         print("LedOff")
+
+
