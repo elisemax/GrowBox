3fe3f4c025d3bd8a862e92a9c0607c52e1365263
Dima <dzmitry.rabotkin@woltair.cz>
2022-07-03 16:16:31 +0200

    blue

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -1,20 +1,40 @@
 
 import bluetooth
 
+# def receiveMessages():
+#   server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  
+#   port = 1
+#   server_sock.bind(("",port))
+#   server_sock.listen(1)
+#   server_sock
+  
+  
+#   client_sock,address = server_sock.accept()
+#   print ("Accepted connection from " + str(address))
+  
+#   data = client_sock.recv(1024)
+#   print ("received [%s]" % data)
+  
+#   client_sock.close()
+#   server_sock.close()
 def receiveMessages():
-  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-  
-  port = 1
-  server_sock.bind(("",port))
-  server_sock.listen(1)
-  server_sock
-  
-  
-  client_sock,address = server_sock.accept()
-  print ("Accepted connection from " + str(address))
-  
-  data = client_sock.recv(1024)
-  print ("received [%s]" % data)
-  
-  client_sock.close()
-  server_sock.close()
+  hostMACAddress = 'B8:27:EB:C9:EF:55' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+  port = 3
+  backlog = 1
+  size = 1024
+  s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+  s.bind((hostMACAddress, port))
+  s.listen(backlog)
+  try:
+      client, clientInfo = s.accept()
+      while 1:
+          data = client.recv(size)
+          if data:
+              print(data)
+              client.send(data) # Echo back to client
+              client.send('idi nahoi')
+  except:	
+      print("Closing socket")
+      client.close()
+      s.close()
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while(True):
 
         systemGrow = ad.Adapter(False)
 
-        time.sleep(1)
+        time.sleep(5)
         #tJsonObj = rGet.text
 
         #systemGrow.phUpdate()
@@ -34,20 +34,21 @@ while(True):
         #rGet = requests.get('http://192.168.0.10:8080/grow/status',json=systemGrow.Json_Obj())
         
         #systemGrow.getTime()
-        #bluetooth.bluetooth.receiveMessages()
+        bluetooth.bluetooth.receiveMessages()
         #print(rGet.status_code)
         #g = git.cmd.Git('https://github.com/elisemax/GrowBox.git')
         #g.pull()
         
-        jsonObj = systemGrow.createJsonObj(systemGrow.controlUnitId,
-        systemGrow.getLightsRealStatus,
-        systemGrow.getTemperature,systemGrow.getPH,
-        systemGrow.getPPM,systemGrow.getHum,
-        systemGrow.getTime)
-        rPut = requests.post('http://p5023.dev.inited.cz/api/test/',data=jsonObj,headers=headers)
-        print('http://p5023.dev.inited.cz/api/test/'+systemGrow.controlUnitId)
-        print(jsonObj)
-        print(str(rPut.json))
+        #jsonObj = systemGrow.createJsonObj(systemGrow.controlUnitId,
+        #systemGrow.getLightsRealStatus,
+        #systemGrow.getTemperature,systemGrow.getPH,
+        #systemGrow.getPPM,systemGrow.getHum,
+        #systemGrow.getTime
+        #rPut = requests.post('http://p5023.dev.inited.cz/api/test/',data=jsonObj,headers=headers)
+        #print('http://p5023.dev.inited.cz/api/test/'+systemGrow.controlUnitId)
+        #print(jsonObj)
+        #print(str(rPut.json))
+
         
         
     #except Exception:
