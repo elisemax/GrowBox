3ad3c6907d034bc9a4b26621d752a6943a304274
Dima <dzmitry.rabotkin@woltair.cz>
2022-07-05 16:02:31 +0200

    c

--- wifi/bluetoosh.py
+++ wifi/bluetoosh.py
@@ -26,22 +26,25 @@ class Bluetoosh:
         size = 1024
         s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         s.bind((hostMACAddress, port))
+       # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen(backlog)
      #   bluetooth.advertise_service(s,'hui', '1e0ca4ea-299d-4335-93eb-27fcfe7fa848')
         os.system('sudo hciconfig hci0 piscan')
         try:
-            print('waitong for bluetooth connection..')
+            print('started connecting')
             client, clientInfo = s.accept()
             while 1:
+                print('waitong for bluetooth connection..')
                 data = client.recv(size)
                 if data:
+                    print("Polu4ili data po bluetooth")
                     print(data)
                     client.send(data) # Echo back to client
-                    client.send('idi nahoi')
+                    client.send('Otpravili data nazad')
                     #interface = 'wlan0'
                     #name = ‘Wifi’
                     #password = ‘password’
-                    os.system('iwconfig ' + interface + ' essid ' + name + ' key ' + password)
+                    #os.system('iwconfig ' + interface + ' essid ' + name + ' key ' + password)
         except:	
             print("Closing socket")
             client.close()
