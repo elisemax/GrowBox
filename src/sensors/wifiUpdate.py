3403fcacb052432dcc264bf1560f98f9e73bcefd
Max <max11cz@seznam.cz>
2022-07-16 15:27:05 +0200

    dew

--- src/sensors/wifiUpdate.py
+++ src/sensors/wifiUpdate.py
@@ -3,5 +3,5 @@ def update_wifi(file,ssid,password):
     contents = raw.read().split("\n")
     raw.seek(0)                        # <- This is the missing piece
     raw.truncate()
-    raw.write('country=CZ\nctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n update_config=1\n network={\n ssid="{ssid}"\npsk="{password}"\n}')
+    raw.write('country=CZ\nctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n ssid="{ssid}"\npsk="{password}"\n}')
     raw.close()
