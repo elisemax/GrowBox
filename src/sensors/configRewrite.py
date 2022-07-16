b422a977b2a91f7a4558ac363f9dfccfcdfbd5dc
Max <max11cz@seznam.cz>
2022-07-16 15:31:38 +0200

    dwe

--- src/sensors/configRewrite.py
+++ src/sensors/configRewrite.py
@@ -1,5 +1,6 @@
 import subprocess
+import os
 def fileRewrite():
     #subprocess.call('sudo su');
-    subprocess.call('sudo cp -f /home/admin/grow/GrowBox/src/service/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf');
-    subprocess.call('reboot');
+    subprocess.call('sudo cp -f /home/admin/grow/GrowBox/src/service/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf',shell=True);
+    subprocess.call('reboot',shell=True);
