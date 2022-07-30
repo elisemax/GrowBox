1bf133eec21031bb4a67eac3ffe7374ef9954f49
Dima <dzmitry.rabotkin@woltair.cz>
2022-07-30 15:20:54 +0200

    b

--- src/sensors/waterSensors.py
+++ src/sensors/waterSensors.py
@@ -15,7 +15,7 @@ class WaterSensors:
     def ec_read_voltage():
         
         adc = Adafruit_ADS1x15.ADS1015()
-        value = adc.read_adc( )
+        value = adc.read_adc(1)
         analog_voltage = value * (4.096 / 2047)
         avg = analog_voltage
         print("Ec")
