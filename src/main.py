d306873972c3e985a5e8dd21ebc14ce5fb9fbfe8
Dima <dzmitry.rabotkin@woltair.cz>
2022-08-06 19:15:27 +0200

    bbb

--- src/main.py
+++ src/main.py
@@ -15,13 +15,11 @@ import git
 headers = {"Content-Type":"application/json", "Content-Length":"16","Host":"p5023.dev.inited.cz"}
 while(True): 
 
-    #try:  
+    try:  
 
     
         #systemGrow.connectToWifi()
-    Bluetoosh.receiveMessages()
-        
-    systemGrow = ad.Adapter(True,True,True,True)
+    
     
     #False, False, False, False
     #True,True,True,True
@@ -42,21 +40,28 @@ while(True):
     #"humidity3": systemGrow.hum3,
     #"humidity4": systemGrow.hum4,
     #"ppm": systemGrow.ec },headers=headers)
-    if(systemGrow.hum1==True or systemGrow.hum2==True or systemGrow.hum3 == True or systemGrow.hum4 == True or systemGrow.waterlevel == False):
-        print("turning off pump")
-        systemGrow.setPump(False)
-    else:
-        print("turning on pump")
-        systemGrow.setPump(True)
-    
-    print("ec: ")
-    print(systemGrow.ec)
-    
-    
-    print("ph")
-    print(systemGrow.ph)
-    
-    time.sleep(2)
+        Bluetoosh.receiveMessages()
+            
+        systemGrow = ad.Adapter(True,True,True,True)
+        if(systemGrow.hum1==True or systemGrow.hum2==True or systemGrow.hum3 == True or systemGrow.hum4 == True or systemGrow.waterlevel == False):
+            print("turning off pump")
+            systemGrow.setPump(False)
+        else:
+            print("turning on pump")
+            systemGrow.setPump(True)
+        
+        print("ec: ")
+        print(systemGrow.ec)
+        
+        
+        print("ph")
+        print(systemGrow.ph)
+        
+        time.sleep(2)
+        
+    except Exception as e:
+            print(e)
+            pass
    
    
 
