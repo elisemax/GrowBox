4f68ed698365828e4507fb1757f0c9701fb1ab79
Dima <dzmitry.rabotkin@woltair.cz>
2022-08-06 17:07:16 +0200

    fff

--- src/sensors/led.py
+++ src/sensors/led.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 
 class Led:
-    def __init__(self, mode):
+    def __init__(self, first, second, third, fourth):
         self.__ledStatus = self.ledControl(mode)
         
         
@@ -11,47 +11,47 @@ class Led:
         return self.__ledStatus
     
     
-    def ledControl(self, On):
-        if On == True:
-            self.LedOff()
-            return True
-        
-        if On == False:
-            self.LedOn()
-            return False
-        
-        
-    def LedOff(self):
-        GPIO.setmode(GPIO.BCM)
+    def ledControl(self, first, second, third, fourth):
         OutputPin1 = 13
         OutputPin2 = 19
         OutputPin3 = 5
         OutputPin4 = 6
-        GPIO.setup(OutputPin1, GPIO.OUT)
-        GPIO.setup(OutputPin2, GPIO.OUT)
-        GPIO.setup(OutputPin3, GPIO.OUT)
-        GPIO.setup(OutputPin4, GPIO.OUT)
-        GPIO.setwarnings(False)
-        GPIO.output(OutputPin1, GPIO.HIGH)
-        GPIO.output(OutputPin2, GPIO.HIGH)
-        GPIO.output(OutputPin3, GPIO.HIGH)
-        GPIO.output(OutputPin4, GPIO.HIGH)
+        
+        pins = {
+        "first": 13,
+        "second": 19,
+        "third": 5,
+        "fourth": 6
+        }
+        self.setLight(13, first)
+        self.setLight(19, second)
+        self.setLight(5, third)
+        self.setLight(6, fourth)
         
         
-    def LedOn(self):
+    def setLight(self, pin, value):
+        if value == True:
+            self.LedOff(pin)
+        if value == False:
+            self.LedOn(pin)
+        
+    def LedOff(self, outputPin):
         GPIO.setmode(GPIO.BCM)
-        OutputPin1 = 13
-        OutputPin2 = 19
-        OutputPin3 = 5
-        OutputPin4 = 6
-        GPIO.setup(OutputPin1, GPIO.OUT)
-        GPIO.setup(OutputPin2, GPIO.OUT)
-        GPIO.setup(OutputPin3, GPIO.OUT)
-        GPIO.setup(OutputPin4, GPIO.OUT)
+        
+        GPIO.setup(outputPin, GPIO.OUT)
+    
         GPIO.setwarnings(False)
-        GPIO.output(OutputPin1, GPIO.LOW)
-        GPIO.output(OutputPin2, GPIO.LOW)
-        GPIO.output(OutputPin3, GPIO.LOW)
-        GPIO.output(OutputPin4, GPIO.LOW)
+        GPIO.output(outputPin, GPIO.HIGH)
+        
+        
+        
+    def LedOn(self, outputPin):
+        GPIO.setmode(GPIO.BCM)
+       
+        GPIO.setup(outputPin, GPIO.OUT)
+    
+        GPIO.setwarnings(False)
+        GPIO.output(outputPin, GPIO.LOW)
+       
 
 
