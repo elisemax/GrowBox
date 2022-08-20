f59876366c869efc527f8f1cd16ed258365a4ba5
Dima <dzmitry.rabotkin@woltair.cz>
2022-08-20 16:07:07 +0200

    n

--- src/sensors/configRewrite.py
+++ src/sensors/configRewrite.py
@@ -1,6 +1,11 @@
 import subprocess
 import os
+import getpass
 def fileRewrite():
+    
+    username = getpass.getuser()
+    print('username:')
+    print(username)
     #subprocess.call('sudo su');
-    subprocess.call('sudo cp -f /home/admin/GrowBox/src/service/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf',shell=True);
+    subprocess.call('sudo cp -f /home/' + username + '/GrowBox/src/service/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf',shell=True);
     subprocess.call('reboot',shell=True);
