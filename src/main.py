d9e0cc08d090cf0fecdec6a3e16048d69bf0ba07
Dima <dzmitry.rabotkin@woltair.cz>
2022-08-21 13:12:52 +0200

    v

--- src/main.py
+++ src/main.py
@@ -71,8 +71,9 @@ else:
         
     print("ph")
     print(systemGrow.ph)
-    txt_log = "ec = "+ systemGrow.ec + "ph = " + systemGrow.ph
-
+    
+    
+txt_log = "ec = "+ systemGrow.ec + "ph = " + systemGrow.ph
 os.system('echo ' + txt_log + ' >> /home/frm04/crontest.txt')
             
         
